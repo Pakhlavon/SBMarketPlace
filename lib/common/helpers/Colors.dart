bcf4bdbcd5260c9af4e2c89885d44152df3b0329
pakhlavon <Pahlavon-9596@mail.ru>
2024-05-23 17:51:49 +0500

    removed build apk file

---
+++ lib/common/helpers/Colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'hex_color.dart';
+
+class MyColors {
+  MyColors._();
+
+  static const Color white = Colors.white;
+  static Color blue = HexColor("#005AFC");
+  static Color mainBlue = HexColor("#076AE2");
+  static Color green = HexColor("#41C270");
+  static Color black = Colors.black;
+  static Color DoveGray = HexColor("#6E6E6E");
+  static Color grayLight = HexColor("#E6E7E8");
+  static Color textGrayLight = HexColor("#80858E");
+  static Color deepOrange = Colors.deepOrange;
+}
